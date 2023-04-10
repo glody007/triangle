8c77b4fe7bdef437b917face062a3d2074e98bd4
glody007 <glodymbutwile@gmail.com>
2023-04-10 17:28:08 +0200

    feat: Show screens based on auth hook.

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigation from './navigation/MainNavigation';
+import { AuthProvider } from './hooks/useAuth';
 
 export default function App() {
+
   return (
     <NavigationContainer>
-      <MainNavigation />
+      {/* HOC - Higher Order Component */}
+      <AuthProvider>
+        <MainNavigation />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,12 +1,24 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
+import ChatScreen from '../screens/ChatScreen';
+import LoginScreen from '../screens/LoginScreen';
+import useAuth from "../hooks/useAuth"
 
 const Stack = createNativeStackNavigator()
 
 export default function MainNavigation() {
+  const { user } = useAuth()
+
   return (
     <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        {user ? (
+          <>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="ChatScreen" component={ChatScreen} />
+          </>
+        ) : (
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        )}
     </Stack.Navigator>
   )
 }
