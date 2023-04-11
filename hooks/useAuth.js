cfe1da7a97f00b385f4f6821a1ef2ee6e048d92f
glody007 <glodymbutwile@gmail.com>
2023-04-11 12:58:19 +0200

    feat: Add google auth.

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,16 +1,63 @@
-import { View, Text } from 'react-native'
-import React, { createContext, useContext } from 'react'
+import React, { 
+    createContext, 
+    useContext,
+    useState,
+    useEffect
+} from 'react'
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { 
+    IOS_CLIENT_ID,
+    ANDROID_CLIENT_ID,
+    EXPO_CLIENT_ID
+} from '@env'
+
+WebBrowser.maybeCompleteAuthSession();
 
 const AuthContext = createContext({})
 
 export function AuthProvider({ children }) {
-  return (
-    <AuthContext.Provider value={{
-        user: null
-    }}>
-      {children}
-    </AuthContext.Provider>
-  )
+    const [token, setToken] = useState("");
+    const [userInfo, setUserInfo] = useState(null);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        iosClientId: IOS_CLIENT_ID,
+        androidClientId: ANDROID_CLIENT_ID,
+        expoClientId: EXPO_CLIENT_ID
+    })
+
+    useEffect(() => {
+        if (response?.type === "success") {
+            setToken(response.authentication.accessToken);
+            getUserInfo();
+        }
+    }, [response, token]);
+
+    const getUserInfo = async () => {
+        try {
+          const response = await fetch(
+            "https://www.googleapis.com/userinfo/v2/me",
+            {
+              headers: { Authorization: `Bearer ${token}` },
+            }
+          );
+    
+          const user = await response.json();
+          setUserInfo(user);
+        } catch (error) {
+          // Add error handler here
+        }
+    };
+
+    return (
+        <AuthContext.Provider value={{
+            user: userInfo,
+            toke: token,
+            googleAuth: () => promptAsync({ useProxy: true })
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 
 export default function useAuth() {
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import useAuth from '../hooks/useAuth'
 
 export default function LoginScreen() {
+    const { user, googleAuth } = useAuth()
 
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+    return (
+        <View className="flex-1 justify-center items-center">
+            <TouchableOpacity 
+                onPress={() => googleAuth()}
+                className="bg-blue-500 px-20 py-5 rounded-full"
+            >
+                <Text className="text-white text-xl font-bold">Login</Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
