91cb4dce34e55eaf84c805d8196ed51ad7c922f5
glody007 <glodymbutwile@gmail.com>
2023-04-12 08:55:40 +0200

    feat: Login screen design.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,18 +1,39 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { 
+    View, 
+    Text, 
+    TouchableOpacity,
+    ImageBackground
+} from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import useAuth from '../hooks/useAuth'
+import { useNavigation } from '@react-navigation/native'
 
 export default function LoginScreen() {
+    const navigation = useNavigation()
     const { user, googleAuth } = useAuth()
 
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, [])
+
     return (
-        <View className="flex-1 justify-center items-center">
-            <TouchableOpacity 
-                onPress={() => googleAuth()}
-                className="bg-blue-500 px-20 py-5 rounded-full"
+        <View className="flex-1">
+            <ImageBackground
+                resizeMode='cover' 
+                className="flex-1 items-center"
+                source={{
+                    uri: "https://tinder.com/static/tinder.png"
+                }}
             >
-                <Text className="text-white text-xl font-bold">Login</Text>
-            </TouchableOpacity>
+                <TouchableOpacity 
+                    onPress={() => googleAuth()}
+                    className="bg-white px-20 py-5 rounded-2xl absolute bottom-40"
+                >
+                    <Text className="font-semibold">Sign in</Text>
+                </TouchableOpacity>
+            </ImageBackground>
         </View>
     )
 }
