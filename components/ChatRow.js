2977f4f36dfe5bfee0c767a94f8539ca641ad220
glody007 <glodymbutwile@gmail.com>
2023-04-21 16:54:31 +0200

    feat: Show last message.

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -4,15 +4,43 @@ import {
     Image,
     TouchableOpacity
 } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import getMatchedUser from '../lib/getMatchedUser'
 import useAuth from '../hooks/useAuth'
+import { collection, limit, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { db } from '../firebase'
 
 export default function ChatRow({ matchedUsers }) {
     const navigation = useNavigation()
     const { user } = useAuth()
     const matchedUser = getMatchedUser(matchedUsers, user.uid)
+    const [lastMessageData, setLastMessageData] = useState('')
+
+    useEffect(() => {
+        let unsub;
+
+        const fetchLastMessage = async () => {
+            unsub = onSnapshot(
+                query(
+                    collection(db, 'matches', matchedUsers.id, 'messages'),
+                    orderBy("timestamp", "desc"),
+                    limit(1)
+                ), 
+                (snapshot) => {
+                    const messages = snapshot.docs
+                                        .map(doc => ({
+                                            id: doc.id,
+                                            ...doc.data()
+                                        })) 
+                    if(messages?.length > 0) setLastMessageData(messages[0])
+
+                })
+        }
+
+        fetchLastMessage()
+        return unsub
+    }, [])
 
     return (
         <TouchableOpacity 
@@ -32,7 +60,7 @@ export default function ChatRow({ matchedUsers }) {
             </View>
             <View className="border-b border-gray-300 h-full w-full justify-center">
                 <Text className="text-xl font-bold">{matchedUser.displayName}</Text>
-                <Text className="text-gray-500">last message</Text>
+                <Text className="text-gray-500">{lastMessageData.message}</Text>
             </View>
         </TouchableOpacity>
     )
